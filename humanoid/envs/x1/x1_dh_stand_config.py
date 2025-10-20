a56c79250b9ac757c85b18e41ae8df808b2128d8
lrm <lirunmo0919@gmail.com>
2025-10-20 16:06:50 +0800

    熵0.005直线重训

--- humanoid/envs/x1/x1_dh_stand_config.py
+++ humanoid/envs/x1/x1_dh_stand_config.py
@@ -78,6 +78,7 @@ class X1DHStandCfg(LeggedRobotCfg):
         mesh_type = 'plane'
         # mesh_type = 'trimesh'
         curriculum = True
+        # curriculum = False
         # rough terrain only:
         measure_heights = False
         static_friction = 0.6
@@ -88,26 +89,26 @@ class X1DHStandCfg(LeggedRobotCfg):
         num_cols = 20  # number of terrain cols (types)
         max_init_terrain_level = 5  # starting curriculum state
         platform = 3.
-        terrain_dict = {"flat": 1.0, 
-                        "rough flat": 0.0,
-                        "slope up": 0.0,
-                        "slope down": 0.0, 
-                        "rough slope up": 0.0,
-                        "rough slope down": 0.0, 
-                        "stairs up": 0.0, 
-                        "stairs down": 0.0,
-                        "discrete": 0.0, 
-                        "wave": 0.0,}
-        # terrain_dict = {"flat": 0.3, 
-        #                 "rough flat": 0.2,
-        #                 "slope up": 0.2,
-        #                 "slope down": 0.2, 
+        # terrain_dict = {"flat": 1.0, 
+        #                 "rough flat": 0.0,
+        #                 "slope up": 0.0,
+        #                 "slope down": 0.0, 
         #                 "rough slope up": 0.0,
         #                 "rough slope down": 0.0, 
         #                 "stairs up": 0.0, 
         #                 "stairs down": 0.0,
-        #                 "discrete": 0.1, 
+        #                 "discrete": 0.0, 
         #                 "wave": 0.0,}
+        terrain_dict = {"flat": 0.3, 
+                        "rough flat": 0.2,
+                        "slope up": 0.2,
+                        "slope down": 0.2, 
+                        "rough slope up": 0.0,
+                        "rough slope down": 0.0, 
+                        "stairs up": 0.0, 
+                        "stairs down": 0.0,
+                        "discrete": 0.1, 
+                        "wave": 0.0,}
         terrain_proportions = list(terrain_dict.values())
 
         rough_flat_range = [0.005, 0.01]  # meter
@@ -287,15 +288,15 @@ class X1DHStandCfg(LeggedRobotCfg):
         joint_viscous_range = [0.05, 0.1]
         
     class commands(LeggedRobotCfg.commands):
-        curriculum = False
-        # curriculum = True
+        # curriculum = False
+        curriculum = True
         max_curriculum = 1.5
         # Vers: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
         num_commands = 4
         resampling_time = 25.  # time before command are changed[s]
         # gait = ["walk_omnidirectional","stand","walk_omnidirectional"] # gait type during training
-        gait = ["stand"] # gait type during training
-        # gait = ["walk_sagittal","stand"] # gait type during training
+        # gait = ["stand"] # gait type during training
+        gait = ["walk_sagittal","stand"] # gait type during training
         # proportion during whole life time
         gait_time_range = {"walk_sagittal": [2,6],
                            "walk_lateral": [2,6],
@@ -345,8 +346,8 @@ class X1DHStandCfg(LeggedRobotCfg):
             # contact 
             feet_contact_forces = -0.01
             # vel tracking
-            tracking_lin_vel = 1.8
-            tracking_ang_vel = 1.1
+            tracking_lin_vel = 2.2
+            tracking_ang_vel = 1.2
             vel_mismatch_exp = 0.5  # lin_z; ang x,y
             low_speed = 0.2
             track_vel_hard = 0.5
@@ -400,7 +401,7 @@ class X1DHStandCfgPPO(LeggedRobotCfgPPO):
         in_channels = X1DHStandCfg.env.frame_stack
 
     class algorithm(LeggedRobotCfgPPO.algorithm):
-        entropy_coef = 0.001
+        entropy_coef = 0.005
         learning_rate = 1e-5
         num_learning_epochs = 2
         gamma = 0.994
@@ -415,14 +416,14 @@ class X1DHStandCfgPPO(LeggedRobotCfgPPO):
         policy_class_name = 'ActorCriticDH'
         algorithm_class_name = 'DHPPO'
         num_steps_per_env = 24   # per iteration
-        max_iterations = 2000  # number of policy updates
+        max_iterations = 20000  # number of policy updates
 
         # logging
         save_interval = 100  # check for potential saves every this many iterations
         experiment_name = 'x1_dh_stand'
         run_name = ''
         # load and resume
-        resume = False
+        resume = True
         load_run = -1  # -1 = last run
         checkpoint = -1  # -1 = last saved model
         resume_path = None  # updated from load_run and chkpt
